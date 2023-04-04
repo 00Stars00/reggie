63376b8bbe54ae861add1f911da7edba27489f07
艾克拜尔 <1500547933@qq.com>
2023-04-04 18:22:29 +0800

    菜品管理模块-新增菜品-接受页面提交的数据-代码开发

--- src/main/java/org/example/reggie/controller/DishController.java
+++ src/main/java/org/example/reggie/controller/DishController.java
@@ -1,14 +1,20 @@
 package org.example.reggie.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.example.reggie.common.R;
+import org.example.reggie.dto.DishDto;
 import org.example.reggie.service.DishFlavorService;
 import org.example.reggie.service.DishService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 菜品管理
  */
+@Slf4j
 @RestController
 @RequestMapping("/dish")
 public class DishController {
@@ -18,6 +24,21 @@ public class DishController {
 
     @Autowired
     private DishFlavorService dishFlavorService;
+
+
+    /**
+     * 添加菜品
+     * @param dishDto 菜品信息
+     * @return 成功信息
+     */
+    @PostMapping
+    public R<String> save(@RequestBody DishDto dishDto) {
+
+        log.info("添加菜品: {}", dishDto);
+
+        // 添加菜品
+        return null;
+    }
 
 
 
