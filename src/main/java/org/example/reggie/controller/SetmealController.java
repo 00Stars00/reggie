85a3c486957b17f8a13ae5e2c721696928945594
艾克拜尔 <1500547933@qq.com>
2023-04-05 12:00:09 +0800

    套餐管理模块-创建结构

---
+++ src/main/java/org/example/reggie/controller/SetmealController.java
@@ -0,0 +1,25 @@
+package org.example.reggie.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.example.reggie.service.SetmealDishService;
+import org.example.reggie.service.SetmealService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 套餐管理
+ */
+@Slf4j
+@RestController
+@RequestMapping("/setmeal")
+public class SetmealController {
+
+    @Autowired
+    private SetmealService setmealService;
+
+    @Autowired
+    private SetmealDishService setmealDishService;
+
+}
